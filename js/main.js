e23733d2536c1d1bc5bb1e8a689efa085c76c2bf
ramaeb <bianucciramiro@hotmail.com>
2025-10-25 17:21:36 -0300

    modified:   css/styles.css 	modified:   index.html 	new file:   inspowebRef/TablaDashboardEJEMPLO.png 	modified:   js/main.js 	new file:   productos.html

--- js/main.js
+++ js/main.js
@@ -2,14 +2,42 @@
 const barraBusqueda = document.getElementById("input-nombre");
 const botonIngresar = document.getElementById("boton-ingresar");
 
+const menuToggle = document.getElementById("menu-toggle");
+const menuLinks = document.getElementById("menu-links");
+const divSaludoBienvenida = document.getElementById("div-saludo-bienvenida");
+const nombreUsuarioGuardado = localStorage.getItem("nombreUsuario");
+
+//Responsive para cerrar y abrir el menu navegacion desde el celu...
+menuToggle.addEventListener("click", () => {menuLinks.classList.toggle("active");});
+
+// Cerrar al hacer clic en un enlace
+document.querySelectorAll("#menu-links a").forEach(link => {
+  link.addEventListener("click", () => {menuLinks.classList.remove("active");});
+});
+
+
+function mostrarSaludoBienvenida(){
+    if(nombreUsuarioGuardado){
+        divSaludoBienvenida.innerHTML = `<p>¡Bienvenido, ${nombreUsuarioGuardado}!</p>`;
+    }
+}
+
+
+
+
 /*Funcion para ingresar en nombre*/
 function ingresarNombre(){
     let nombreUsuario = barraBusqueda.value;
+    //Faltaria un regex para validar nombres raros o con numeros etc.
     if(nombreUsuario == "" || nombreUsuario == ""){
-        alert("Por favor, ingresa un nombre válido.");
+        alert("Por favor, ingresa un nombre válido."); 
     }
     else{
         alert("¡Bienvenido, " + nombreUsuario + "!");
+        //Redirecciona a la pagina de productos (Vemos si hacemos un innerhtml y usamos la misma pagina)
+        window.location.href = "productos.html";
+        nombreUsuarioGuardado = localStorage.setItem("nombreUsuario", nombreUsuario);
     }
 
 }
+mostrarSaludoBienvenida();
